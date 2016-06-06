bb8f9e90dae804d47366636c2727601aa9d9e09e
micha <micha1989@bluewin.ch>
2016-06-06 17:44:58 -0300

    Small changes in Return class

--- src/Utils/Return.java
+++ src/Utils/Return.java
@@ -1,28 +1,42 @@
 package Utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Return {
-	private ArrayList<String> arl;
+	private HashMap<String, String> errorMap;
+
 	private boolean success;
 	
 
 	
 	public Return(){
 		setSuccess(true);
-		arl = new ArrayList<String>();
+		errorMap = new HashMap<String, String>();
 	}
 	
 	
 	public void addError(String errorMsg){
 		setSuccess(false);
-		arl.add(errorMsg);
+		
+		errorMap.put("error",errorMsg);
 	}
-	public ArrayList<String> getErrors(){return arl;}
+
+	public void addError(String tableName, String attributeName, String errorMsg){
+		setSuccess(false);
+		
+		errorMap.put("atrb_"+attributeName,errorMsg);
+	}
+	
+	
+	public HashMap<String, String> getErrors(){return errorMap;}
 	
 	
 	public boolean isSuccess(){return success;}
-	public void setSuccess(boolean success){this.success = success;}
+	public void setSuccess(boolean success){
+		this.success = success;
+		this.errorMap = new HashMap<>();
+	}
 	
 	
 }

46677983be76defe4b9361b82f2c4c2b45d4d48c
Micha Meier <micha1989@bluewin.ch>
2016-09-12 10:21:50 -0300

    Add Date/String converters

--- src/converters/DateToString.java
+++ src/converters/DateToString.java
@@ -1,6 +1,6 @@
 package converters;
 
-import java.sql.Date;
+import java.util.Date;
 
 import interfaces.ISimpleConverter;
 
@@ -9,8 +9,25 @@ public class DateToString implements ISimpleConverter {
 	@SuppressWarnings("deprecation")
 	@Override
 	public Object convert(Object entry) throws Exception {
-		Date d = (Date)entry;
-		return d.getDay()+"/"+d.getMonth()+"/"+d.getYear();
+		Date date = (Date)entry;
+		Integer iy = new Integer(date.getYear());
+		Integer im = new Integer(date.getMonth());
+		Integer id = new Integer(date.getDate());
+		
+		
+		String d = getStringNumber(id);
+		String m = getStringNumber(im);
+		String y = iy.toString();
+		
+		return d+"/"+m+"/"+y;
+	}
+	
+	
+	private String getStringNumber(Integer value){
+		if(value < 10)
+			return "0"+value.toString();
+		else
+			return value.toString();
 	}
 
 }

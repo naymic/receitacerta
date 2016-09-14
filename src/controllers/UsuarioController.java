3cb56df9a0f996caa3f9ace7ee4e03bb049205da
thix <thiagomwd@gmail.com>
2016-09-14 20:40:38 -0300

    Altercoes no arquivo index e adicao arquivo queromecadastrar.html

--- src/controllers/UsuarioController.java
+++ src/controllers/UsuarioController.java
@@ -1,7 +1,18 @@
 package controllers;
 
-public class UsuarioController extends CrudController {
+import jsonclasses.JReturn;
+import model.Model;
 
+public class UsuarioController extends CrudController {
+	
+	public JReturn novoAction(JReturn r, Model object){
+		return r;
+	}
+	
+	public JReturn salvarAction(JReturn r, Model object){
+		return super.salvarAction(r, object);
+	}
+	
 	@Override
 	public boolean needAuthentication(){
 		return false;

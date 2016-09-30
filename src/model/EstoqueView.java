6f5ac480f62892b42fcd3512ab120d4e15442804
Micha Meier <micha1989@bluewin.ch>
2016-09-30 15:40:45 -0300

    Make serch in estoque work

--- src/model/EstoqueView.java
+++ src/model/EstoqueView.java
@@ -8,9 +8,13 @@ import annotations.Entity;
 public class EstoqueView extends EstoqueParent {
 	
 	String ingredientesId;
+	
+	@Override
+	public String getTableName() {
+		return "view_estoque";
+	}
 
-
-	@Entity(attributeName="ingredientes_id", fk=true)
+	@Entity(attributeName="ingredientes_id")
 	public String dgetIngredientesId() {
 		return ingredientesId;
 	}

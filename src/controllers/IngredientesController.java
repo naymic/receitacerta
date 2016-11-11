074a37bf2b6e0419f407522ee1a626713cb60520
Micha Meier <micha1989@bluewin.ch>
2016-11-11 20:55:21 -0200

    2.Add new Ingredientes controller

--- src/controllers/IngredientesController.java
+++ src/controllers/IngredientesController.java
@@ -1,8 +1,12 @@
 package controllers;
 
+import java.util.ArrayList;
+
 import annotations.AControllerMethod;
 import dao.DAO;
+import dao.DAORelation;
 import jresponseclasses.JReturn;
+import model.Ingredientes;
 import model.Model;
 
 public class IngredientesController extends CrudController {
@@ -11,9 +15,23 @@ public class IngredientesController extends CrudController {
 	@Override
 	@AControllerMethod(checkAttributes = true, needAuthentication = true)
 	public JReturn salvarAction(JReturn r, Model object) {
+		Ingredientes obj = (Ingredientes)object;
 		
 		
-		return super.salvarAction(r, object);
+		Ingredientes ingr = new Ingredientes();
+		ingr.dsetNome(obj.dgetNome());
+		ingr.dsetIngredientesUnidade(ingr.dgetIngredientesUnidade());
+		
+		ArrayList<Model> listIngredientes = DAORelation.getInstance().select(ingr);
+		if(listIngredientes.size()==0){
+			return super.salvarAction(r, object);	
+		}else{
+			r.addSimpleError("Ingrediente com essa unidade já existente!");
+		}
+		
+		return r;
+		
+		
 	}
 	
 	
